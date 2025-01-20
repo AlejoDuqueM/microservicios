1253c74ad6a2954d0bc5d4ab8bba86cb4378741a
AlejoDuqueM <alejomudu@hotmail.com>
2025-01-20 14:55:44 -0500

    actualizacion primer microservicio

--- Primer_Programa/cursoSpringBoot/src/main/java/cursoSpringBoot/CursoConSpringBootApplication.java
+++ Primer_Programa/cursoSpringBoot/src/main/java/cursoSpringBoot/CursoConSpringBootApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CursoConSpringBootApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(CursoConSpringBootApplication.class, args);
+
 	}
 
 }
