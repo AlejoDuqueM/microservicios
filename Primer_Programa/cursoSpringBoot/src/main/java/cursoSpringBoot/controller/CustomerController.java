e45985c0a89a6ef49d6594683221ad2046c79ba3
AlejoDuqueM <alejomudu@hotmail.com>
2025-01-20 21:54:10 -0500

    Crud Finalizado

--- Primer_Programa/cursoSpringBoot/src/main/java/cursoSpringBoot/controller/CustomerController.java
+++ Primer_Programa/cursoSpringBoot/src/main/java/cursoSpringBoot/controller/CustomerController.java
@@ -2,8 +2,7 @@ package cursoSpringBoot.controller;
 
 
 import cursoSpringBoot.model.Customer;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -13,10 +12,15 @@ import java.util.List;
 public class CustomerController {
 
     private List<Customer> customers = new ArrayList<>(Arrays.asList(
+
             new Customer(1, "John Doe", "johndoe", "123456"),
+
             new Customer(2, "Jane Doe", "janedoe", "123456"),
+
             new Customer(3, "Mary Jane", "maryjane", "123456"),
+
             new Customer(4, "Peter Parker", "peterparker", "123456")
+
     ));
 
     @GetMapping("/clientes")
@@ -26,7 +30,71 @@ public class CustomerController {
 
     }
 
+    @GetMapping("/clientes/{username}")
+    public Customer getCustomers(@PathVariable String username) {
 
+        for(Customer c : customers) {
 
+            if(c.getUsername().equalsIgnoreCase(username)) {
+
+                return c;
+
+            }
+
+        }
+
+        return null;
+    }
+
+    @PostMapping("/clientes")
+    public Customer postCustomer(@RequestBody Customer customer) {
+
+        customers.add(customer);
+
+        return customer;
+
+    }
+
+    @PutMapping("/clientes")
+    public Customer putCustomer(@RequestBody Customer customer) {
+
+        for(Customer c : customers) {
+
+            if(c.getId() == customer.getId()) {
+
+                c.setName(customer.getName());
+
+                c.setUsername(customer.getUsername());
+
+                c.setPassword(customer.getPassword());
+
+                return c;
+
+            }
+
+        }
+
+        return null;
+
+    }
+
+    @DeleteMapping("/clientes/{id}")
+    public Customer deleteCustomer(@PathVariable int id) {
+
+        for(Customer c : customers) {
+
+            if(c.getId() == id) {
+
+                customers.remove(c);
+
+                return c;
+
+            }
+
+        }
+
+        return null;
+
+    }
 
 }
