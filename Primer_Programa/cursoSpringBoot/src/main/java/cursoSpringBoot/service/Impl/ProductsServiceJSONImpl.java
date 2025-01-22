4a8181d37265d75143198fe0e99adc9999ce5e75
AlejoDuqueM <alejomudu@hotmail.com>
2025-01-21 22:23:58 -0500

    JSON serializable y desserializable, java-json serializable

---
+++ Primer_Programa/cursoSpringBoot/src/main/java/cursoSpringBoot/service/Impl/ProductsServiceJSONImpl.java
@@ -0,0 +1,46 @@
+package cursoSpringBoot.service.Impl;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import cursoSpringBoot.model.Product;
+import cursoSpringBoot.service.IProductsService;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.util.List;
+
+//Primary es una anotacion que se usa para indicar
+//que esta es la implementacion por defecto de la interfaz
+//Esta clase lee desde el archivo products.json
+@Primary
+@Service
+public class ProductsServiceJSONImpl implements IProductsService {
+
+
+    //Jackson usa un constructor vacio para crear una instancia de la clase
+    @Override
+    public List<Product> getAllProducts() {
+
+        List<Product> products;
+
+        try {
+
+            //Leer archivo JSON
+            //Convertir JSON a List<Product>
+
+            products = new ObjectMapper()
+                    .readValue(this.getClass().getResourceAsStream("/products.json"),
+                            new TypeReference<List<Product>>() {});
+
+            //Retornar List<Product>
+            return products;
+
+        }catch (IOException e) {
+
+            throw new RuntimeException(e);
+
+        }
+
+    }
+}
